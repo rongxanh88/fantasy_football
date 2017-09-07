0663f0dbf84b12971181103dc665d3bc24a09cc2
rongxanh88 <rongxanh88@gmail.com>
2017-09-07 14:22:34 -0600

    receivers now filtered by injured players and inactives

--- app/controllers/api/v1/receivers_controller.rb
+++ app/controllers/api/v1/receivers_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::ReceiversController < ApplicationController
   def index
-    @receivers = FootballPlayer.where(position: "WR")
+    # @receivers = FootballPlayer.where(position: "WR")
+    @receivers = FootballPlayer.receivers
   end
 end
