5b309a46a01ccd418a47abd4c62beda7f946175c
rongxanh88 <rongxanh88@gmail.com>
2017-09-09 09:20:50 -0600

    created successfull request testing for all current API endpoints

--- spec/factories/defenses.rb
+++ spec/factories/defenses.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :defense do
     sequence(:api_id) { |n| "#{n}" }
-    name "DEN"
+    sequence(:name) { |n| "defense#{n}" }
     expected_point_production "30.0"
   end
 end
