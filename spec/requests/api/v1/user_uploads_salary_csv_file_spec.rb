d1553338fd829614429ddd17bde526edf13dd551
rongxanh88 <rongxanh88@gmail.com>
2017-09-16 14:51:17 -0600

    skipped testing for uploading csv salary data

--- spec/requests/api/v1/user_uploads_salary_csv_file_spec.rb
+++ spec/requests/api/v1/user_uploads_salary_csv_file_spec.rb
@@ -3,9 +3,10 @@ require "rails_helper"
 RSpec.describe "Salary CSV Upload", :type => :request do
 
   context "successful upload" do
-    it "updates salary data for all players" do
+    xit "updates salary data for all players" do
+      #csv_file_location = "../../../data/DKSalaries.csv"
+
 
     end
-
   end
 end
